788d7ab74cb8cb546c35b2ea843f6e41258a9d83
Arpit <rpit.arora89@gmail.com>
2015-05-23 13:03:39 +0100

    Adding a new Project to Repo

---
+++ SpringTime/src/main/java/Spring/SpringTrial/App.java
@@ -0,0 +1,13 @@
+package Spring.SpringTrial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
